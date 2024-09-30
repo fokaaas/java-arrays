6d895c0478e4d2f566462b9c0b697d42a61e7dcd
Stanislav Basarab <stanislavbasarab083@gmail.com>
2024-09-30 14:50:48 +0300

    feat(Main): implemented method for sum columns elements

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         int[][] matrix1 = {
                 {1, 2, 3},
-                {4, 5}
+                {4, 5, 6}
         };
 
         int[][] matrix2 = {
@@ -13,42 +13,76 @@ public class Main {
                 {11, 12}
         };
 
+        int[][] result = new int[0][];
+
         try {
-            int[][] result = multiplyMatrices(matrix1, matrix2);
-            System.out.println("Result:");
+            result = multiplyMatrices(matrix1, matrix2);
+            System.out.println("Result of matrix multiplication:");
             printMatrix(result);
         } catch (IllegalArgumentException e) {
-            System.out.println("Помилка: " + e.getMessage());
+            System.out.println("Error during matrix multiplication: " + e.getMessage());
         }
+
+        int sum = sumOfMaxElementsInColumns(result);
+        System.out.println("Sum of the largest elements of each column: " + sum);
     }
 
-    private static int[][] multiplyMatrices(int[][] matrixA, int[][] matrixB) throws IllegalArgumentException {
+    public static int[][] multiplyMatrices(int[][] matrixA, int[][] matrixB) throws IllegalArgumentException {
         if (matrixA[0].length != matrixB.length) {
-            throw new IllegalArgumentException("The number of columns of the first matrix must be equal to the number of rows of the second matrix.");
+            throw new IllegalArgumentException("The number of columns of the matrix A must be equal to the number of rows of the matrix B.");
         }
+
+        if (isNotRectangularMatrix(matrixA) || isNotRectangularMatrix(matrixB)) {
+            throw new IllegalArgumentException("The matrices must be rectangular.");
+        }
+
         int rowsA = matrixA.length;
         int colsA = matrixA[0].length;
         int colsB = matrixB[0].length;
 
-        int[][] resultMatrix = new int[rowsA][colsB];
+        int[][] result = new int[rowsA][colsB];
 
         for (int i = 0; i < rowsA; i++) {
             for (int j = 0; j < colsB; j++) {
                 for (int k = 0; k < colsA; k++) {
-                    resultMatrix[i][j] += matrixA[i][k] * matrixB[k][j];
+                    result[i][j] += matrixA[i][k] * matrixB[k][j];
                 }
             }
         }
 
-        return resultMatrix;
+        return result;
     }
 
-    private static void printMatrix(int[][] matrix) {
+    public static void printMatrix(int[][] matrix) {
         for (int[] row : matrix) {
             for (int element : row) {
                 System.out.print(element + " ");
             }
             System.out.println();
         }
+    }
+
+    public static boolean isNotRectangularMatrix(int[][] matrix) {
+        int length = matrix[0].length;
+        for (int[] row : matrix) {
+            if (row.length != length) return true;
+        }
+        return false;
+    }
+
+    public static int sumOfMaxElementsInColumns(int[][] matrix) throws IllegalArgumentException {
+        int cols = matrix[0].length;
+        int sum = 0;
+
+        for (int j = 0; j < cols; j++) {
+            int max = Integer.MIN_VALUE;
+            for (int[] ints : matrix) {
+                if (ints[j] > max) {
+                    max = ints[j];
+                }
+            }
+            sum += max;
+        }
+        return sum;
     }
 }
