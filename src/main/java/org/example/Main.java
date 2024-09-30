912557617c92c1cd88118e782a60f329e29caeeb
Stanislav Basarab <stanislavbasarab083@gmail.com>
2024-09-30 14:14:04 +0300

    feat(Main): added method for matrix multiplication

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,53 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        int[][] matrix1 = {
+                {1, 2, 3},
+                {4, 5}
+        };
 
+        int[][] matrix2 = {
+                {7, 8},
+                {9, 10},
+                {11, 12}
+        };
+
+        try {
+            int[][] result = multiplyMatrices(matrix1, matrix2);
+            System.out.println("Result:");
+            printMatrix(result);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Помилка: " + e.getMessage());
+        }
+    }
+
+    private static int[][] multiplyMatrices(int[][] matrixA, int[][] matrixB) throws IllegalArgumentException {
+        if (matrixA[0].length != matrixB.length) {
+            throw new IllegalArgumentException("The number of columns of the first matrix must be equal to the number of rows of the second matrix.");
+        }
+        int rowsA = matrixA.length;
+        int colsA = matrixA[0].length;
+        int colsB = matrixB[0].length;
+
+        int[][] resultMatrix = new int[rowsA][colsB];
+
+        for (int i = 0; i < rowsA; i++) {
+            for (int j = 0; j < colsB; j++) {
+                for (int k = 0; k < colsA; k++) {
+                    resultMatrix[i][j] += matrixA[i][k] * matrixB[k][j];
+                }
+            }
+        }
+
+        return resultMatrix;
+    }
+
+    private static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int element : row) {
+                System.out.print(element + " ");
+            }
+            System.out.println();
+        }
     }
 }
